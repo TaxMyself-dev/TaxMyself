80c77c334bca462de15f9966b486ccc6cf8f62a7
Shmuel Harel <info@taxmyself.co.il>
2024-05-02 22:19:51 +0300

    updated responsive of item nevigation part two

--- frontend/src/app/pages/reports/reports.page.ts
+++ frontend/src/app/pages/reports/reports.page.ts
@@ -8,10 +8,10 @@ import { IItemNavigate } from 'src/app/shared/interface';
 })
 export class ReportsPage implements OnInit {
  
-  itemsNavigate: IItemNavigate[] = [{ name: 'דו"ח מעמ', link: "/vat-report", icon: "document-outline", id: '0' },
-                                    { name:  'דו"ח רווח והפסד', link: "/income-statement", icon: "document-outline", id: '1' }, 
-                                    { name: 'דו"ח שנתי', link: "/annual-report", icon: "document-outline", id:'2'}, 
-                                    { name: 'דו"ח מקדמות למס הכנסה', link: "/advance-income-tax-report", icon: "document-outline", id: '3' }];
+  itemsNavigate: IItemNavigate[] = [{ name: 'דו"ח מעמ', link: "/vat-report", icon: "document-outline", id: '0', index: 'zero'},
+                                    { name:  'דו"ח רווח והפסד', link: "/income-statement", icon: "document-outline", id: '1', index: 'one'}, 
+                                    { name: 'דו"ח שנתי', link: "/annual-report", icon: "document-outline", id:'2', index: 'two'}, 
+                                    { name: 'דו"ח מקדמות למס הכנסה', link: "/advance-income-tax-report", icon: "document-outline", id: '3', index: 'three'}];
 
   constructor() { }
 
