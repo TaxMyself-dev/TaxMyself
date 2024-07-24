a2be5750a4e808e1ecd94d40f90a6e7c5215cde5
shovalHarel <shovalraviv@gmail.com>
2024-07-24 19:44:35 +0300

    added default error message to required fields

--- frontend/src/app/shared/generic-input/generic-input.component.ts
+++ frontend/src/app/shared/generic-input/generic-input.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, Input, OnChanges, OnInit, SimpleChanges } from '@angular/core';
 import { FormGroup } from '@angular/forms';
 
 @Component({
@@ -6,15 +6,23 @@ import { FormGroup } from '@angular/forms';
   templateUrl: './generic-input.component.html',
   styleUrls: ['./generic-input.component.scss', '../shared-styling.scss'],
 })
-export class GenericInputComponent  implements OnInit {
+export class GenericInputComponent implements OnChanges {
 
   @Input() parentForm: FormGroup;
   @Input() controlName: string;
   @Input() inputLabel: string;
-  @Input() errorText = "שדה זה הוא חובה";
+  @Input() errorText: string;
+  @Input() isRequired = false;
+
+  RequiredErrorMessage = "שדה זה הוא חובה";
+  errorMessage: string;
 
   constructor() { }
 
-  ngOnInit() {}
+  ngOnChanges(changes: SimpleChanges): void {
+    if (changes.errorText || changes.isRequired) {
+      this.errorMessage = this.isRequired ? this.errorText ?? this.RequiredErrorMessage : this.errorText;
+    }
+  }
 
 }
