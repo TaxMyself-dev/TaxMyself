a79b610f476db0e23852b3d40c1a43e5ba139ad1
Shmuel Harel <info@taxmyself.co.il>
2025-11-05 21:18:30 +0200

    fix storage bucket

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -120,7 +120,7 @@ export class AppModule {
     clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
     privateKey: process.env.FIREBASE_PRIVATE_KEY?.replace(/\\n/g, '\n'),
   }),
-  storageBucket: 'taxmyself-5d8a0.appspot.com',
+  storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
 });
   }
 }
